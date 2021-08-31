fd9ffa0499995fef0de14f1569a3691ebe5beec2
Brendan Duke <brendanw.duke@gmail.com>
2021-08-30 20:20:55 -0400

    JS in browser

---
+++ js-in-browser/script.js
@@ -0,0 +1,12 @@
+4 + 3;
+if ((4 + 3) === 7) {
+  console.log("Hellooooo");
+  alert("You're smart!");
+}
+
+console.log("Hellooooo");
+console.log("Hellooooo");
+console.log("Hellooooo");
+console.log("Hellooooo");
+console.log("Hellooooo");
+console.log("Hellooooo");
